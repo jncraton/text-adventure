14f6faf2530543325b2873275b9415ff6cdc9efa
Jon Craton <jncraton@gmail.com>
2023-09-15 13:09:53 -0400

    Add template

---
+++ adventure.py
@@ -0,0 +1,9 @@
+# Disaplay the start of your narritive to the user
+
+# Ask the user to choose between a number of options
+
+# Check the user submission and follow different narrative paths based
+# on their answer
+
+# Continue your narrative providing additional decision points
+# Make sure that you include the appropriate everything specified
